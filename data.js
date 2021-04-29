d822ef836309df5748481902d351d1edba8e7fe5
Prosper Nsengiyumva <n.prosper@gmail.com>
2021-04-29 12:53:28 -0600

    mpac
    mpac

---
+++ data.js
@@ -0,0 +1,227 @@
+var geojson = {
+    type: 'FeatureCollection',
+    features: [
+    {
+        type: 'Feature',
+        geometry: {
+        type: 'Point',
+        coordinates: [-114.08914, 51.08613]
+        },
+        properties: {
+            "name": "Alberta Bible College",
+            "website": "https://www.abccampus.ca/",
+            "long": -114.08914,
+            "lat": 51.08613,
+            "type": "u",
+            "enrollment": "90,000",
+            "averagecost": "$5000",
+            "acceptance": "92%",
+            "totalprograms": 100,
+            "averageprogramlength": "3 years",
+            "description": "Alberta Bible College's Mission: To equip people for effective service & witness for Christ"
+        }
+    },
+    {
+        type: 'Feature',
+        geometry: {
+            type: 'Point',
+            coordinates: [-114.09256, 51.064331]
+        },
+        properties: {
+            "name": "Alberta College of Art and Design",
+            "website": "https://www.auarts.ca/",
+            "long": -114.09256,
+            "lat": 51.064331,
+            "type": "c",
+            "enrollment": "100,000",
+            "averagecost": "$4500",
+            "acceptance": "89%",
+            "totalprograms": 25,
+            "averageprogramlength": "2.5 years",
+            "description": "The Alberta University of the Arts is a public university of art and design located in Calgary, Alberta, Canada. The university's beginnings date back to the founding of the Provincial Institute of Technology and Art in 1916. "
+        }
+    },
+    {
+        type: 'Feature',
+        geometry: {
+            type: 'Point',
+            coordinates: [-114.19257, 51.03402]
+        },
+        properties: {
+            "name": "Ambrose University College",
+            "website": "https://ambrose.edu/",
+            "long": -114.19257,
+            "lat": 51.03402,
+            "type": "c",
+            "enrollment": "100,000",
+            "averagecost": "$4501",
+            "acceptance": "89%",
+            "totalprograms": 25,
+            "averageprogramlength": "2.5 years",
+            "description": "The Alberta University of the Arts is a public university of art and design located in Calgary, Alberta, Canada. The university's beginnings date back to the founding of the Provincial Institute of Technology and Art in 1916. "
+}
+    },
+    {
+    type: 'Feature',
+    geometry: {
+        type: 'Point',
+        coordinates: [-114.057968, 51.04731]
+    },
+    properties: {"name": "Bow Valley College",
+    "website": "https://bowvalleycollege.ca/",
+    "long": -114.057968,
+    "lat": 51.04731,
+    "type": "c",
+    "enrollment": "100,000",
+    "averagecost": "$4502",
+    "acceptance": "89%",
+    "totalprograms": 25,
+    "averageprogramlength": "2.5 years",
+    "description": "The Alberta University of the Arts is a public university of art and design located in Calgary, Alberta, Canada. The university's beginnings date back to the founding of the Provincial Institute of Technology and Art in 1916. "
+    }
+},
+{
+type: 'Feature',
+        geometry: {
+            type: 'Point',
+            coordinates: [-113.737061, 52.48793]
+        },
+        properties: { "name": "Canadian University College",
+"website": "https://www.burmanu.ca/",
+"long": -113.737061,
+"lat": 52.48793,
+"type": "c",
+"enrollment": "100,000",
+"averagecost": "$4503",
+"acceptance": "89%",
+"totalprograms": 25,
+"averageprogramlength": "2.5 years",
+"description": "The Alberta University of the Arts is a public university of art and design located in Calgary, Alberta, Canada. The university's beginnings date back to the founding of the Provincial Institute of Technology and Art in 1916. "
+}},
+{
+type: 'Feature',
+        geometry: {
+            type: 'Point',
+            coordinates: [-113.44512, 53.55942]
+        },
+        properties: {"name": "Concordia University College of Alberta",
+"website": "https://concordia.ab.ca/",
+"long": -113.44512,
+"lat": 53.55942,
+"type": "c",
+"enrollment": "100,000",
+"averagecost": "$4504",
+"acceptance": "89%",
+"totalprograms": 25,
+"averageprogramlength": "2.5 years",
+"description": "The Alberta University of the Arts is a public university of art and design located in Calgary, Alberta, Canada. The university's beginnings date back to the founding of the Provincial Institute of Technology and Art in 1916. "
+}},
+{
+type: 'Feature',
+        geometry: {
+            type: 'Point',
+            coordinates: [-118.8198, 55.17553]
+        },
+        properties: {"name": "Grande Prairie Regional College",
+"website": "http://www.yahoo.com",
+"long": -118.8198,
+"lat": 55.17553,
+"type": "c",
+"enrollment": "100,000",
+"averagecost": "$4506",
+"acceptance": "89%",
+"totalprograms": 25,
+"averageprogramlength": "2.5 years",
+"description": "The Alberta University of the Arts is a public university of art and design located in Calgary, Alberta, Canada. The university's beginnings date back to the founding of the Provincial Institute of Technology and Art in 1916. "
+}},
+{
+type: 'Feature',
+        geometry: {
+            type: 'Point',
+            coordinates: [-114.19257, 51.03402]
+        },
+        properties: {"name": "Ambrose University College",
+"website": "https://ambrose.edu/",
+"long": -114.19257,
+"lat": 51.03402,
+"type": "c",
+"enrollment": "100,000",
+"averagecost": "$4501",
+"acceptance": "89%",
+"totalprograms": 25,
+"averageprogramlength": "2.5 years",
+"description": "The Alberta University of the Arts is a public university of art and design located in Calgary, Alberta, Canada. The university's beginnings date back to the founding of the Provincial Institute of Technology and Art in 1916. "
+}},
+{
+type: 'Feature',
+        geometry: {
+            type: 'Point',
+            coordinates: [-114.057968, 51.04731]
+        },
+        properties: { "name": "Bow Valley College",
+"website": "https://bowvalleycollege.ca/",
+"long": -114.057968,
+"lat": 51.04731,
+"type": "c",
+"enrollment": "100,000",
+"averagecost": "$4502",
+"acceptance": "89%",
+"totalprograms": 25,
+"averageprogramlength": "2.5 years",
+"description": "The Alberta University of the Arts is a public university of art and design located in Calgary, Alberta, Canada. The university's beginnings date back to the founding of the Provincial Institute of Technology and Art in 1916. "
+}},
+{
+type: 'Feature',
+        geometry: {
+            type: 'Point',
+            coordinates: [-113.737061, 52.48793]
+        },
+        properties: {"name": "Canadian University College",
+"website": "https://www.burmanu.ca/",
+"long": -113.737061,
+"lat": 52.48793,
+"type": "c",
+"enrollment": "100,000",
+"averagecost": "$4503",
+"acceptance": "89%",
+"totalprograms": 25,
+"averageprogramlength": "2.5 years",
+"description": "The Alberta University of the Arts is a public university of art and design located in Calgary, Alberta, Canada. The university's beginnings date back to the founding of the Provincial Institute of Technology and Art in 1916. "
+}},
+{
+type: 'Feature',
+        geometry: {
+            type: 'Point',
+            coordinates: [-113.44512, 53.55942]
+        },
+        properties: {"name": "Concordia University College of Alberta",
+"website": "https://concordia.ab.ca/",
+"long": -113.44512,
+"lat": 53.55942,
+"type": "c",
+"enrollment": "100,000",
+"averagecost": "$4504",
+"acceptance": "89%",
+"totalprograms": 25,
+"averageprogramlength": "2.5 years",
+"description": "The Alberta University of the Arts is a public university of art and design located in Calgary, Alberta, Canada. The university's beginnings date back to the founding of the Provincial Institute of Technology and Art in 1916. "
+}},
+{
+type: 'Feature',
+        geometry: {
+            type: 'Point',
+            coordinates: [-118.8198, 55.17553]
+        },
+        properties: {"name": "Grande Prairie Regional College",
+"website": "http://www.yahoo.com",
+"long": -118.8198,
+"lat": 55.17553,
+"type": "c",
+"enrollment": "100,000",
+"averagecost": "$4506",
+"acceptance": "89%",
+"totalprograms": 25,
+"averageprogramlength": "2.5 years",
+"description": "The Alberta University of the Arts is a public university of art and design located in Calgary, Alberta, Canada. The university's beginnings date back to the founding of the Provincial Institute of Technology and Art in 1916. "
+}}]
+};
---
+++ index.js
@@ -0,0 +1,76 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiY29kZWRyaXZlbiIsImEiOiJja20wdDIzY3oyOHA0Mm9zNWJmazhlOTMwIn0.8MntRE4biSPIUE8z6-odvQ';
+var map = new mapboxgl.Map({
+    container: 'map', // container id
+    style: 'mapbox://styles/codedriven/ckm0u5lzw3qfy17lk7lbb1mdp', // style URL
+    center: [-114.07116744127545, 51.01951879728364], // starting position [lng, lat]
+    zoom: 7 // starting zoom
+});
+
+map.addControl(new mapboxgl.NavigationControl());
+
+// add markers to map
+geojson.features.forEach(function(marker) {
+    // create a HTML element for each feature
+    var el = document.createElement('div');
+    el.className = 'marker';
+    // make a marker for each feature and add to the map
+    new mapboxgl.Marker(el).setLngLat(marker.geometry.coordinates).addTo(map); 
+    new mapboxgl.Marker(el)
+        .setLngLat(marker.geometry.coordinates)
+        .setPopup(new mapboxgl.Popup({ offset: 25 })
+        .setHTML('<h3><a href="' + marker.properties.website + '">' +  marker.properties.name + '</a></h3>' 
+            + '<p>' + marker.properties.description + '</p>'
+            + '<p><strong>Type: </strong>' + (marker.properties.type === "u" ? "University" : "College") +'<br />'
+            + '<strong>Acceptance Rate: </strong>' + marker.properties.acceptance +'<br />'
+            + '<strong>Enrollment: </strong>' + marker.properties.enrollment +'<br />'
+            + '<strong>Total Programs: </strong>' + marker.properties.totalprograms +'<br />'
+            + '<strong>Average program length: </strong>' + marker.properties.averageprogramlength +'<br />'
+            + '<strong>Average cost: </strong>' + marker.properties.averagecost +' (CAD)</p>'))
+        .addTo(map);
+});
+
+// Custom layer implemented as ES6 class
+class NullIslandLayer {
+    constructor() {
+        this.id = 'null-island';
+        this.type = 'custom';
+        this.renderingMode = '3d';
+    }
+
+    onAdd(map, gl) {
+        const vertexSource = `
+        uniform mat4 u_matrix;
+        void main() {
+            gl_Position = u_matrix * vec4(0.5, 0.5, 0.0, 1.0);
+            gl_PointSize = 20.0;
+        }`;
+
+        const fragmentSource = `
+        void main() {
+            gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+        }`;
+
+        const vertexShader = gl.createShader(gl.VERTEX_SHADER);
+        gl.shaderSource(vertexShader, vertexSource);
+        gl.compileShader(vertexShader);
+        const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+        gl.shaderSource(fragmentShader, fragmentSource);
+        gl.compileShader(fragmentShader);
+
+        this.program = gl.createProgram();
+        gl.attachShader(this.program, vertexShader);
+        gl.attachShader(this.program, fragmentShader);
+        gl.linkProgram(this.program);
+    }
+
+    render(gl, matrix) {
+        gl.useProgram(this.program);
+        gl.uniformMatrix4fv(gl.getUniformLocation(this.program, "u_matrix"), false, matrix);
+        gl.drawArrays(gl.POINTS, 0, 1);
+    }
+}
+
+map.on('load', function() {
+    console.log('this is a test');
+    map.addLayer(new NullIslandLayer());
+});
